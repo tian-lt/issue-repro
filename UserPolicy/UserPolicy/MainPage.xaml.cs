123c5cf87c0ff502caf8150e641e546f6837b4f8
tian-lt <tilia@microsoft.com>
2022-02-23 16:17:08 +0800

    UserPolicy

---
+++ UserPolicy/UserPolicy/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Management.Policies;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace UserPolicy
+{
+    public sealed partial class MainPage : Page
+    {
+        public int RefreshedCount
+        {
+            get { return (int)GetValue(RefreshedCountProperty); }
+            set { SetValue(RefreshedCountProperty, value); }
+        }
+
+        public string GraphingModeState
+        {
+            get { return (string)GetValue(GraphingModeStateProperty); }
+            set { SetValue(GraphingModeStateProperty, value); }
+        }
+
+        public static readonly DependencyProperty RefreshedCountProperty =
+            DependencyProperty.Register("RefreshedCount", typeof(int), typeof(MainPage), new PropertyMetadata(0));
+
+        public static readonly DependencyProperty GraphingModeStateProperty =
+            DependencyProperty.Register("GraphingModeState", typeof(string), typeof(MainPage), new PropertyMetadata("Unknown"));
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _ = Dispatcher.RunAsync(CoreDispatcherPriority.High, async () =>
+            {
+                var usr = ((App)Application.Current).CurrentUser;
+                while (true)
+                {
+                    var policy = NamedPolicy.GetPolicyFromPathForUser(usr, "Education", "AllowGraphingCalculator");
+                    GraphingModeState = policy.GetBoolean() ? "Enabled" : "Disabled";
+                    ++RefreshedCount;
+                    await Task.Delay(TimeSpan.FromSeconds(0.5));
+                }
+            });
+        }
+
+        public static Brush GreenIf(string state)
+        {
+            return new SolidColorBrush(state == "Enabled" ? Colors.Green : Colors.Red);
+        }
+    }
+}
